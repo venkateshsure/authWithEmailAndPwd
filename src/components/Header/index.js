e65ad11482f123c13c4bb26684ddf30a8844c8b9
venkateshsure <venky007sure@gmail.com>
2024-02-25 15:31:47 +0530

    need to inplement the navbar

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+
+import { FaUserCircle } from "react-icons/fa";
+
+import './index.css'
+
+
+const Header=()=>{
+    return (
+        <div className="header-con">
+            <div className="header-first-con">
+                <FaUserCircle/>
+                <h4>navbar</h4>    
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,20 +2,51 @@ import { signOut } from "firebase/auth"
 
 import {auth} from '../../firebase'
 
+import {useState,useEffect} from 'react'
+
+import Header from '../Header'
+
+
+import './index.css'
+
 const Home=()=>{
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(userCredential=> {
+            console.log(userCredential)
+            const {displayName} = userCredential;
+            
+            if (displayName) {
+                setUser(displayName);
+        
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
 
     const onSignOut=async ()=>{
         await signOut(auth)
     }
 
-
     return (
-        <div>
-            <center>
-                <h1>welcome user</h1>
-                <button onClick={onSignOut}>Sign out</button>
-            </center>
+        <>
+        <Header/>
+        <div className="home-con">
+
+            <div className="fir-con">
+    
+                
+                    <h1 className="text-head">Hi! {user}</h1>
+                    <button onClick={onSignOut}>Sign out</button>
+                
+            </div>
         </div>
+        </>
     )
 }
 
